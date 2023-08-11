28098c25774d628cb5146d9a86b180e983f549b2
Ekemini Samuel <favourekemini@yahoo.com>
2023-08-11 18:10:35 +0100

    go-url-short

--- main.go
+++ main.go
@@ -4,14 +4,47 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"strings"
 	"time"
 )
 
-type URLShortener struct {
-	urls map[string]string
+var urls = make(map[string]string)
+
+func main() {
+	http.HandleFunc("/", handleForm)
+	http.HandleFunc("/shorten", handleShorten)
+	http.HandleFunc("/short/", handleRedirect)
+
+	fmt.Println("URL Shortener is running on :3030")
+	http.ListenAndServe(":3030", nil)
 }
 
-func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
+func handleForm(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		http.Redirect(w, r, "/shorten", http.StatusSeeOther)
+		return
+	}
+
+	// Serve the HTML form
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>URL Shortener</title>
+		</head>
+		<body>
+			<h2>URL Shortener</h2>
+			<form method="post" action="/shorten">
+				<input type="url" name="url" placeholder="Enter a URL" required>
+				<input type="submit" value="Shorten">
+			</form>
+		</body>
+		</html>
+	`)
+}
+
+func handleShorten(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
@@ -23,27 +56,46 @@ func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Generate a unique shortened key for the original URL
 	shortKey := generateShortKey()
-	us.urls[shortKey] = originalURL
+	urls[shortKey] = originalURL
 
-	shortenedURL := fmt.Sprintf("http://localhost:8080/short/%s", shortKey)
-	fmt.Fprintf(w, "Shortened URL: %s\n", shortenedURL)
+	// Construct the full shortened URL
+	shortenedURL := fmt.Sprintf("http://localhost:3030/short/%s", shortKey)
+
+	// Serve the result page
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>URL Shortener</title>
+		</head>
+		<body>
+			<h2>URL Shortener</h2>
+			<p>Original URL: `, originalURL, `</p>
+			<p>Shortened URL: <a href="`, shortenedURL, `">`, shortenedURL, `</a></p>
+		</body>
+		</html>
+	`)
 }
 
-func (us *URLShortener) HandleRedirect(w http.ResponseWriter, r *http.Request) {
-	shortKey := r.URL.Path[len("/short/"):]
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	shortKey := strings.TrimPrefix(r.URL.Path, "/short/")
 	if shortKey == "" {
 		http.Error(w, "Shortened key is missing", http.StatusBadRequest)
 		return
 	}
 
-	originalURL, found := us.urls[shortKey]
+	// Retrieve the original URL from the `urls` map using the shortened key
+	originalURL, found := urls[shortKey]
 	if !found {
 		http.Error(w, "Shortened key not found", http.StatusNotFound)
 		return
 	}
 
-	http.Redirect(w, r, originalURL, http.StatusFound)
+	// Redirect the user to the original URL
+	http.Redirect(w, r, originalURL, http.StatusMovedPermanently)
 }
 
 func generateShortKey() string {
@@ -56,16 +108,4 @@ func generateShortKey() string {
 		shortKey[i] = charset[rand.Intn(len(charset))]
 	}
 	return string(shortKey)
-}
-
-func main() {
-	shortener := &URLShortener{
-		urls: make(map[string]string),
-	}
-
-	http.HandleFunc("/shorten", shortener.HandleShorten)
-	http.HandleFunc("/short/", shortener.HandleRedirect)
-
-	fmt.Println("URL Shortener is running on :8080")
-	http.ListenAndServe(":8080", nil)
 }
